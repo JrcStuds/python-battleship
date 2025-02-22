81f9528e53eb37c21702d26a1fabd51bbfff350e
Jacob Chamberlain <jacob.ryan.chamberlain@gmail.com>
2025-02-23 01:54:47 +1100

    place ships

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-global c_board
+global o_board
 global p_board
 
 
-def c_board_init():
-    global c_board
+def o_board_init():
+    global o_board
 
-    c_board = [
+    o_board = [
         ["","","","","","","","","",""],
         ["","","","","","","","","",""],
         ["","","","","","","","","",""],
@@ -20,6 +20,8 @@ def c_board_init():
 
 def p_board_init():
     global p_board
+    ships = [5, 4, 3, 3, 2]
+    print(ships)
 
     p_board = [
         ["","","","","","","","","",""],
@@ -33,6 +35,115 @@ def p_board_init():
         ["","","","","","","","","",""],
         ["","","","","","","","","",""],
     ]
+
+    place_ship(ships, 5)
+    place_ship(ships, 4)
+    place_ship(ships, 3)
+    place_ship(ships, 2)
+
+def place_ship(ships, a):
+    valid_files = ["A","B","C","D","E","F","G","H","I","J",]
+    valid_ranks = ["1","2","3","4","5","6","7","8","9","10",]
+
+    while a in ships:
+        display_board("player_only")
+        print("")
+        display_ships(ships)
+        print("")
+
+        x = input(f"Between which coordinates would you like to place the \"{a} ship\"? (format: \"B2 F2\")\n")
+        # first and second coordinates provided
+        # if no 10 coord
+        if x.index(" ") == 2 and len(x) == 5:
+            y1 = [x[0], x[1]]
+            y2 = [x[3], x[4]]
+        # if 10 coord in first coord
+        elif x.index(" ") == 3 and len(x) == 6:
+            y1 = [x[0], x[1:3]]
+            y2 = [x[4], x[5]]
+        # if 10 coord in second coord
+        elif x.index(" ") == 2 and len(x) == 6:
+            y1 = [x[0], x[1]]
+            y2 = [x[3], x[4:6]]
+        # if both coords have 10
+        elif len(x) == 7:
+            y1 = [x[0], x[1:3]]
+            y2 = [x[4], x[5:7]]
+
+        # first checking coords are valid
+        # if the first characters of each coord are not valid letters
+        if y1[0] not in valid_files or y2[0] not in valid_files:
+            print(1)
+        # if the second characters of each coord are not valid numbers
+        elif y1[1] not in valid_ranks or y2[1] not in valid_ranks:
+            print(2)
+
+        # now checking the actual spacing of the coordinates
+        # NOT if only the x has changed or only the y has changed
+        elif not ((coord(y1)[0] == coord(y2)[0]) ^ (coord(y1)[1] == coord(y2)[1])):
+            print(3)
+            print(f"x {coord(y1)[0] == coord(y2)[0]}  y1 {coord(y1)[0]}  y2 {coord(y2)[0]}")
+            print(f"y {coord(y1)[1] == coord(y2)[1]}  y1 {coord(y1)[1]}  y2 {coord(y2)[1]}")
+        # if the coordinates aren't 5 spaces apart
+        elif not (abs(coord(y1)[0] - coord(y2)[0]) == (a-1) or abs(coord(y1)[1] - coord(y2)[1]) == (a-1)):
+            print(4)
+            print(f"x {abs(coord(y1)[0] - coord(y2)[0]) != 5} {abs(coord(y1)[0] - coord(y2)[0])}")
+            print(f"y {abs(coord(y1)[1] - coord(y2)[1]) != 5} {abs(coord(y1)[1] - coord(y2)[1])}")
+
+        # if all fail checks pass
+        else:
+            # remove a ship from available ships, ending loop
+            ships.pop(ships.index(a))
+
+            # now filling in board spaces
+            # up
+            if coord(y1)[1] < coord(y2)[1]:
+                for i in range(a):
+                    p_board[coord(y1)[1] + i][coord(y1)[0]] = a
+            # down
+            elif coord(y1)[1] > coord(y2)[1]:
+                for i in range(a):
+                    p_board[coord(y1)[1] - i][coord(y1)[0]] = a
+            # left
+            elif coord(y1)[0] > coord(y2)[0]:
+                for i in range(a):
+                    p_board[coord(y1)[1]][coord(y1)[0] - i] = a
+            # right
+            elif coord(y1)[0] < coord(y2)[0]:
+                for i in range(a):
+                    p_board[coord(y1)[1]][coord(y1)[0] + i] = a
+
+
+def coord(x):
+    # split string coordinate into list of 2 coordinates (x, y)
+    y = [x[0], x[1]]
+
+    # for x coordinate, convert letter into number (0 is left, 9 is right)
+    if y[0] == "A":
+        y[0] = 0
+    elif y[0] == "B":
+        y[0] = 1
+    elif y[0] == "C":
+        y[0] = 2
+    elif y[0] == "D":
+        y[0] = 3
+    elif y[0] == "E":
+        y[0] = 4
+    elif y[0] == "F":
+        y[0] = 5
+    elif y[0] == "G":
+        y[0] = 6
+    elif y[0] == "H":
+        y[0] = 7
+    elif y[0] == "I":
+        y[0] = 8
+    elif y[0] == "J":
+        y[0] = 9
+    
+    # for y coordinate, top is 0
+    y[1] = 10 - int(y[1])
+
+    return (y[0], y[1])
 
 def display_board(a):
     if a == "player_only":
@@ -52,7 +163,7 @@ def display_board(a):
             if i == 0:
                 x += f"{10 - i} |"
             else:
-                x += f" {10 - 1} |"
+                x += f" {10 - i} |"
             # repeat 10 times for 10 cells (x coord)
             for j in range(10):
                 if p_board[i][j]:
@@ -92,8 +203,8 @@ def display_board(a):
                 # repeat 10 times for 10 cells (x coord)
                 for k in range(10):
                     # check which board data should come from
-                    if j == 0 and c_board[i][k]:
-                        y += f" {c_board[i][k]} |"
+                    if j == 0 and o_board[i][k]:
+                        y += f" {o_board[i][k]} |"
                     elif j == 1 and p_board[i][k]:
                         y += f" {p_board[i][k]} |"
                     else:
@@ -111,46 +222,48 @@ def display_board(a):
         
         print(letters)
 
-
 def display_ships(a):
+    b = []
+    for i in range(len(a)):
+        b.append(a[i])
+
     # string for printing
     x1 = ""
     x2 = ""
     x3 = ""
     x4 = ""
 
-    if 5 in a:
+    if 5 in b:
         x1 += "   /-----5-----\\"
         x2 += "   \\-----------/"
-        a.pop(a.index(5))
+        b.pop(b.index(5))
     
-    if 4 in a:
+    if 4 in b:
         x1 += "   /----4----\\"
         x2 += "   \\---------/"
-        a.pop(a.index(4))
+        b.pop(b.index(4))
 
     for i in range(2):
-        if 3 in a:
+        if 3 in b:
             x3 += "   /---3---\\"
             x4 += "   \\-------/"
-            a.pop(a.index(3))
+            b.pop(b.index(3))
     
-    if 2 in a:
+    if 2 in b:
         x3 += "   /--2--\\"
         x4 += "   \\-----/"
+        b.pop(b.index(2))
     
     print("SHIPS AVAILABLE:\n")
-    print(x1)
-    print(x2)
-    print("")
+    if x1:
+        print(x1)
+        print(x2)
+        print("")
     print(x3)
     print(x4)
 
 
-c_board_init()
+o_board_init()
 p_board_init()
-display_board("player_only")
 print("")
 display_board("both")
-print("")
-display_ships([5,4,3,3,2])
