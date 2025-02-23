c86bfa323947db2d07665265ca71142e532cf2f2
Jacob Chamberlain <jacob.ryan.chamberlain@gmail.com>
2025-02-23 14:24:19 +1100

    select placed ship

--- main.py
+++ main.py
@@ -60,18 +60,108 @@ def p_board_init():
         ["","","","","","","","","",""],
     ]
 
-    display_board("p_board")
-    print("")
-    display_ships(ships)
-    print("")
-
     while ships:
+        display_board("p_board")
+        print("")
+        display_ships(ships)
+        print("")
+
         x = input("Which ship would you like to place? (number between 2-5)\n")
         if x.isdigit():
             if int(x) in ships:
-                print(3)
-            else: print(2)
-        else: print(1)
+                p_place_ship(ships, int(x))
+            else: print("That ship isn't available")
+        else: print("That ship isn't available")
+        print("")
+
+def p_place_ship(ships, ship_num):
+    # create list of valid inputs for coords
+    valid_files = ["A","B","C","D","E","F","G","H","I","J",]
+    valid_ranks = []
+    for i in range(10): valid_ranks.append(str(i + 1))
+    # toggle for coord check
+    valid_coord = False
+    # first and second coords
+    x1 = ""
+    x2 = ""
+
+    # ask for coords until valid
+    while valid_coord == False:
+        display_board("p_board")
+        print("")
+        display_ships(ships)
+        print("")
+
+        valid_coord = True
+        # input
+        raw_coords = input(f"Between which coordinates would you like to place the \"{ship_num} ship\"? (format: \"B2 F2\")\n")
+
+        # check length of string, only one space, both to avoid errors
+        if len(raw_coords) >= 5 and len(raw_coords) <= 7 and raw_coords.count(" ") == 1:
+            # create temporary first and second coord values to test, coords with 10
+            # no 10 coord
+            if raw_coords.index(" ") == 2 and len(raw_coords) == 5:
+                x1 = [raw_coords[0], raw_coords[1]]
+                x2 = [raw_coords[3], raw_coords[4]]
+            # if 10 coord in first coord
+            elif raw_coords.index(" ") == 3 and len(raw_coords) == 6:
+                x1 = [raw_coords[0], raw_coords[1:3]]
+                x2 = [raw_coords[4], raw_coords[5]]
+            # if 10 coord in second coord
+            elif raw_coords.index(" ") == 2 and len(raw_coords) == 6:
+                x1 = [raw_coords[0], raw_coords[1]]
+                x2 = [raw_coords[3], raw_coords[4:6]]
+            # if both coords have 10
+            elif raw_coords.index(" ") == 3 and len(raw_coords) == 7:
+                x1 = [raw_coords[0], raw_coords[1:3]]
+                x2 = [raw_coords[4], raw_coords[5:7]]
+            else: valid_coord = False
+            
+            # last check of coord formatting
+            if valid_coord == True:
+                # if the first characters of each coord are not valid letters
+                if x1[0] not in valid_files or x2[0] not in valid_files:
+                    valid_coord = False
+                    print("Those aren't valid coordinates")
+                # if the second characters of each coord are not valid numbers
+                elif x1[1] not in valid_ranks or x2[1] not in valid_ranks:
+                    valid_coord = False
+                    print("Those aren't valid coordinates")
+
+                # now checking the actual spacing of the coordinates
+                # NOT if only the x has changed or only the y has changed
+                elif not ((coord(x1)[0] == coord(x2)[0]) ^ (coord(x1)[1] == coord(x2)[1])):
+                    valid_coord = False
+                    print("Those coordinates aren't in a line")
+                # if the coordinates aren't 5 spaces apart
+                elif not (abs(coord(x1)[0] - coord(x2)[0]) == (ship_num-1) or abs(coord(x1)[1] - coord(x2)[1]) == (ship_num-1)):
+                    valid_coord = False
+                    print(f"Those spaces aren't {ship_num} apart")
+                
+                # now actually write the ship in if all fail checks pass
+                else:
+                    # remove ship from ships
+                    ships.remove(ship_num)
+
+                    # up
+                    if coord(x1)[1] < coord(x2)[1]:
+                        for i in range(ship_num):
+                            p_board[coord(x1)[1] + i][coord(x1)[0]] = ship_num
+                    # down
+                    elif coord(x1)[1] > coord(x2)[1]:
+                        for i in range(ship_num):
+                            p_board[coord(x1)[1] - i][coord(x1)[0]] = ship_num
+                    # left
+                    elif coord(x1)[0] > coord(x2)[0]:
+                        for i in range(ship_num):
+                            p_board[coord(x1)[1]][coord(x1)[0] - i] = ship_num
+                    # right
+                    elif coord(x1)[0] < coord(x2)[0]:
+                        for i in range(ship_num):
+                            p_board[coord(x1)[1]][coord(x1)[0] + i] = ship_num
+        else:
+            valid_coord = False
+            print("Invalid coordinate format")
 
 def o_place_ship(a):
     x = False
@@ -123,80 +213,6 @@ def o_place_ship(a):
     
     for i in range(a):
         o_board_hidden[z[i][1]][z[i][0]] = a
-
-def p_place_ship(ships, a):
-    valid_files = ["A","B","C","D","E","F","G","H","I","J",]
-    valid_ranks = ["1","2","3","4","5","6","7","8","9","10",]
-
-    while a in ships:
-        display_board("p_board")
-        print("")
-        display_ships(ships)
-        print("")
-
-        x = input(f"Between which coordinates would you like to place the \"{a} ship\"? (format: \"B2 F2\")\n")
-        # check provided coords are at least 5 chars to prevent errors
-        if len(x) >= 5 and len(x) <= 7 and x.count(" ") == 1:
-            # first and second coordinates provided
-            # if no 10 coord
-            if x.index(" ") == 2 and len(x) == 5:
-                y1 = [x[0], x[1]]
-                y2 = [x[3], x[4]]
-            # if 10 coord in first coord
-            elif x.index(" ") == 3 and len(x) == 6:
-                y1 = [x[0], x[1:3]]
-                y2 = [x[4], x[5]]
-            # if 10 coord in second coord
-            elif x.index(" ") == 2 and len(x) == 6:
-                y1 = [x[0], x[1]]
-                y2 = [x[3], x[4:6]]
-            # if both coords have 10
-            elif len(x) == 7:
-                y1 = [x[0], x[1:3]]
-                y2 = [x[4], x[5:7]]
-
-            # first checking coords are valid
-            # if the first characters of each coord are not valid letters
-            if y1[0] not in valid_files or y2[0] not in valid_files:
-                print(1)
-            # if the second characters of each coord are not valid numbers
-            elif y1[1] not in valid_ranks or y2[1] not in valid_ranks:
-                print(2)
-
-            # now checking the actual spacing of the coordinates
-            # NOT if only the x has changed or only the y has changed
-            elif not ((coord(y1)[0] == coord(y2)[0]) ^ (coord(y1)[1] == coord(y2)[1])):
-                print(3)
-                print(f"x {coord(y1)[0] == coord(y2)[0]}  y1 {coord(y1)[0]}  y2 {coord(y2)[0]}")
-                print(f"y {coord(y1)[1] == coord(y2)[1]}  y1 {coord(y1)[1]}  y2 {coord(y2)[1]}")
-            # if the coordinates aren't 5 spaces apart
-            elif not (abs(coord(y1)[0] - coord(y2)[0]) == (a-1) or abs(coord(y1)[1] - coord(y2)[1]) == (a-1)):
-                print(4)
-                print(f"x {abs(coord(y1)[0] - coord(y2)[0]) != 5} {abs(coord(y1)[0] - coord(y2)[0])}")
-                print(f"y {abs(coord(y1)[1] - coord(y2)[1]) != 5} {abs(coord(y1)[1] - coord(y2)[1])}")
-
-            # if all fail checks pass
-            else:
-                # remove a ship from available ships, ending loop
-                ships.pop(ships.index(a))
-
-                # now filling in board spaces
-                # up
-                if coord(y1)[1] < coord(y2)[1]:
-                    for i in range(a):
-                        p_board[coord(y1)[1] + i][coord(y1)[0]] = a
-                # down
-                elif coord(y1)[1] > coord(y2)[1]:
-                    for i in range(a):
-                        p_board[coord(y1)[1] - i][coord(y1)[0]] = a
-                # left
-                elif coord(y1)[0] > coord(y2)[0]:
-                    for i in range(a):
-                        p_board[coord(y1)[1]][coord(y1)[0] - i] = a
-                # right
-                elif coord(y1)[0] < coord(y2)[0]:
-                    for i in range(a):
-                        p_board[coord(y1)[1]][coord(y1)[0] + i] = a
 
 def coord(x):
     # split string coordinate into list of 2 coordinates (x, y)
@@ -379,6 +395,8 @@ def display_board(a):
             print(line)
         
         print(letters)
+    
+    print("")
 
 def display_ships(a):
     b = []
@@ -426,6 +444,5 @@ def bold(x):
 
 o_board_init()
 p_board_init()
-print("")
 display_board("both")
 display_board("o_board_hidden")
